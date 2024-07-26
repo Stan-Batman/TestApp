48cf9bf94b80f271b5a66e30e8cae62c908d0ccb
Aniket Sinha <aas10120@nyu.edu>
2024-07-25 20:33:53 -0400

    Initial commit

---
+++ TrexisProject/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SimpleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            int age = 0;
+            bool isValidAge = false;
+
+            while (!isValidAge)
+            {
+                Console.Write("Enter your age: ");
+                string ageInput = Console.ReadLine();
+                isValidAge = int.TryParse(ageInput, out age) && age > 0;
+
+                if (!isValidAge)
+                {
+                    Console.WriteLine("Please enter a valid positive integer for age.");
+                }
+            }
+
+            Console.WriteLine($"Hello, {name}! You are {age} years old.");
+        }
+    }
+}
